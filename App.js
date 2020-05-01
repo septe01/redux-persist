43b5c9c451eebed10af4da5ef66a8cac58e4105e
septe.vhirgo@gmail.com <septe.vhirgo@gmail.com>
2020-05-01 10:45:10 +0700

    setUp redux persist

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-
 // Imports: Dependencies
 import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './redux/reducers/index';
 import Counter from './screens/Counter';
 
+// new add by me
+// asyncStoreage move to top
+import {PersistGate} from 'redux-persist/es/integration/react'; //import persistGate from redux-persist integrate/react
+import {persistStore, persistReducer} from 'redux-persist'; //import persistStore for given store and persistReducer for combine redux
+import {createLogger} from 'redux-logger'; //for log every action
 
-const store = createStore(
-  rootReducer
-);
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['counterReducer', 'authReducer'],
+};
+const PersistReducer = persistReducer(persistConfig, rootReducer);
 
-export default App = () => {
+// end new add by me
+
+const store = createStore(PersistReducer, applyMiddleware(createLogger()));
+
+// add again here me
+const PersistStore = persistStore(store);
+// end add again here
+
+export default (App = () => {
   return (
-    <Provider store={store}>    
-        <Counter />   
+    <Provider store={store}>
+      <PersistGate persistor={PersistStore} loading={null}>
+        <Counter />
+      </PersistGate>
     </Provider>
   );
-};
+});
